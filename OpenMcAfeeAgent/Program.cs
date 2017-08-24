c16ef1d10743ef18f0d39b63f0df202eb1ac3383
Erik Morrow <cyborgpencil@gmail.com>
2017-08-24 10:51:34 -0600

    Version 1

--- OpenMcAfeeAgent/Program.cs
+++ OpenMcAfeeAgent/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace OpenMcAfeeAgent
     {
         static void Main(string[] args)
         {
+            // Check system for 32bit or 64bit
+            bool envType = Environment.Is64BitOperatingSystem;
+            string fileLocation32 = "C:\\Program Files\\McAfee\\Agent\\UpdaterUI.exe";
+            string fileLocation64 = "C:\\Program Files\\McAfee\\Agent\\x86\\UpdaterUI.exe";
+
+            // if 32bit
+            if (envType)
+            {
+                ProcessStartInfo mcAfeeUI = new ProcessStartInfo(fileLocation64);
+                mcAfeeUI.CreateNoWindow = true;
+                
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo = mcAfeeUI;
+                    proc.Start();
+                }
+            }
+            else
+            {
+                ProcessStartInfo mcAfeeUI = new ProcessStartInfo(fileLocation32);
+                mcAfeeUI.CreateNoWindow = true;
+
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo = mcAfeeUI;
+                    proc.Start();
+                }
+            }
         }
     }
 }
